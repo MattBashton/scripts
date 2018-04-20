f2a8f83cbb88af94a11ac9d147ee2ec0769ec6e7
Matthew Bashton <matthew.bashton@newcastle.ac.uk>
2018-04-20 15:19:29 +0100

    Updates

--- rsyncFileType.sh
+++ rsyncFileType.sh
@@ -3,10 +3,9 @@
 # Uses rsync to copy over a specific file type from a directory tree to a remote destination
 # Uses GNU parallel to run multiple copy operations at once
 tput bold
-[ $# -ne 4 ] && { echo -en "\nRsync all files in a tree of specific type in parallel\n------------------------------------------------------\n\n - Matthew Bashton 2018\n - This script copies all files from dest to source using rsync and GNU parallel\n\n Error Nothing to do, usage:\n rsynFileType.sh <source dir(will be recursed)> <destination: user@host:path/> <file type to copy (e.g. *.bam)> <number parallel threads to copy multiple files on> \n - Note: requires public key on remote host to work\n\n" ; exit 1; }
-tput sgr0
-
+[ $# -ne 4 ] && { echo -en "\nRsync all files in a tree of specific type in parallel\n------------------------------------------------------\n\n - Matthew Bashton 2018\n - This script copies all files from dest to source using rsync and GNU parallel\n\n Error Nothing to do, usage:\n rsynFileType.sh <source dir(will be recursed)> <destination: user@host:path/> <file type to copy (e.g. bam)> <number parallel threads to copy multiple files on> \n - Note: requires public key on remote host to work\n\n" ; exit 1; }
 set -o pipefail
+tput sgr0
 
 tput bold
 echo -en "Rsync all files in a tree of specific type in parallel \n------------------------------------------------------\n\n - Matthew Bashton 2018\n\n"
@@ -23,70 +22,81 @@ TYPE=$3
 CPU=$4
 
 # Rsync flags to use
-FLAGS="-vlt -e ssh"
+FLAGS="-vvvlth -e ssh"
 
 # Job_list_file
 JL_FILE="rsync_jobs.txt"
 
 # Variables
 echo " "
-echo " * Source path is: $SOURCE"
-echo " * Destination directory is: $DEST"
-echo " * File type to copy from recursed source tree: $TYPE"
-echo " * Number of simluatious transfers: $CPU"
-echo " * Rsync flags: $FLAGS"
-echo " * Rsync jobs written to: $JL_FILE"
+echo " * Source path is: ${SOURCE}"
+echo " * Destination directory is: ${DEST}"
+echo " * File type to copy from recursed source tree: ${TYPE}"
+echo " * Number of simluatious transfers: ${CPU}"
+echo " * Rsync flags: ${FLAGS}"
+echo " * Rsync jobs written to: ${JL_FILE}"
 echo " "
 
 # Get list of source files with find
 tput bold
-echo -ne "Finding all $TYPE files in $SOURCE:\n"
+echo -ne "Finding all ${TYPE} files in ${SOURCE}:\n"
 tput sgr0
 
 # Don't split on white space!
 IFS=$'\n'
 
 # Get out list
-FILE_LIST=( $(find "$SOURCE" -name "$TYPE" -type f | sed -e 's/ /\\ /g') )
+FILE_LIST=( $(find "${SOURCE}" -name "*.${TYPE}" -type f | sed -e 's/ /\\ /g') )
 
 for FILE in ${FILE_LIST[@]}
 do
-    echo "$FILE"
+    echo "${FILE}"
 done
 
 NO_FILES=${#FILE_LIST[@]}
 
 tput bold
-echo -ne "\nFound $NO_FILES to copy\n"
+echo -ne "\nFound ${NO_FILES} to copy\n"
 tput sgr0
 
 # Remove existing jobs file if exists
-if [ -e $JL_FILE ]
+if [ -e ${JL_FILE} ]
 then
-    rm $JL_FILE
+    rm ${JL_FILE}
 fi
+
+# Transfer log produced by rsync
+RSLOGDIR="Rsync_logs"
+
+tput bold
+echo "Creating log dir $RSLOGDIR (will overwrite existing)"
+tput sgr0
+[ -d ${RSLOGDIR} ] && rm -rf ${RSLOGDIR}
+mkdir -p ${RSLOGDIR}
 
 COUNT=1
 
 # Generate job list
 for FILE in ${FILE_LIST[@]}
 do
-    #echo -ne "Makeing job $COUNT of $NO_FILES\n"
-    echo "rsync $FLAGS $FILE $DEST" >> $JL_FILE
+    B_NAME=$(basename "${FILE}" ."{$TYPE}")
+    #echo "Adding $B_NAME to transfer list"
+    LOG_FILE="${RSLOGDIR}/${B_NAME}_log.txt"
+    echo "rsync ${FLAGS} --log-file=${LOG_FILE} ${FILE} ${DEST}" >> $JL_FILE
     ((COUNT++))
 done
 
 tput bold
-echo -ne "\n$NO_FILES jobs written to $JL_FILE\n"
+echo -ne "\n${NO_FILES} jobs written to ${JL_FILE}\n"
 tput sgr0
 
 # Run these
 tput bold
-echo -ne "Setting of $CPU transfer(s) to $SOURCE for $NO_FILES files...\n"
+echo -ne "Setting of $CPU transfer(s) to ${SOURCE} for ${NO_FILES} files...\n"
 tput sgr0
 
-LOG_F="transfer_log.txt"
-parallel --progress --jobs $CPU --joblog $LOG_F < $JL_FILE
+PLOG="parallel_log.txt"
+parallel --progress --jobs ${CPU} --joblog ${PLOG} < ${JL_FILE}
 tput bold
-echo -ne "\nDone! - Run logged to $LOG_F\n"
+echo -ne "\nDone! - Run logged to: $PLOT and Rsync logs are in: $RSLOGDIR\n"
 tput sgr0
